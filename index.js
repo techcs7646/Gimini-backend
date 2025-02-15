4a055901736b85faa63f6bdb88eaea9d6064a0ef
chandrakanta <techcs7646@gmail.com>
2025-02-15 20:31:42 +0530

    updated

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-express = require('express')
-const router = require('./routes'); 
-const app = express()
-const port = 3000
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config()
-const cors = require('cors');
+const express = require("express");
+const router = require("./routes");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+// Use CORS
 app.use(
   cors({
-      origin: "http://localhost:5173", 
-      credentials: true, 
+    origin: "http://localhost:5173", // Change this to your frontend's deployed URL after deployment
+    credentials: true,
   })
 );
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('Connected to MongoDB')
-}).catch((err) => { 
-    console.log('Failed to connect to MongoDB', err)
-})
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
 
 app.use(express.json());
-app.use(router)
+app.use(router);
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// Export the app for Vercel
+module.exports = app;
